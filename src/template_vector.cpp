df9c5246bf9ccda1e92a586076f65b7bae42d644
Brendan Duke <brendanw.duke@gmail.com>
2022-11-23 20:04:24 -0500

    Add template vector example

---
+++ src/template_vector.cpp
@@ -0,0 +1,42 @@
+#include <stdexcept>
+
+template <typename T>
+class Vector {
+private:
+  T *elem;
+  int sz;
+
+public:
+  explicit Vector(int n);
+  ~Vector() { delete[] elem; }
+
+  // copy and move operations...
+
+  T &operator[](int i);
+
+  int size() { return sz; }
+};
+
+template <typename T>
+Vector<T>::Vector(int n) {
+  if (n < 0) {
+    throw std::length_error{"Vector::Vector"};
+  }
+
+  elem = new T[n];
+  sz = n;
+}
+
+template <typename T>
+T &Vector<T>::operator[](int i) {
+  if ((i < 0) || (i >= size())) {
+    throw std::out_of_range("Vector::operator[]");
+  }
+
+  return elem[i];
+}
+
+int main() {
+  Vector<char> vc(200);
+  Vector<double> v(1);
+}
